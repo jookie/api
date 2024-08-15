da8bd075e08fc57c2a77680d1438a9e26187eacb
jookie <dovpeles@gmail.com>
2024-08-15 18:46:07 -0400

    fix commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# /api/main.py
+
+def handler(request, response):
+    with open("data/hello.txt", "w") as file:
+        file.write("step-by-step detailed guide and code")
+    return response.status(200).send("File created successfully!")
+
