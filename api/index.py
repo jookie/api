0907afabe0e0e41ca5905738908cae061864951e
jookie <dovpeles@gmail.com>
2024-08-16 15:04:56 -0400

    fix commit

--- api/index.py
+++ api/index.py
@@ -1,38 +1,24 @@
 from flask import Flask
 from flask_apscheduler import APScheduler
-from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+
+app = Flask(__name__)
+
+# Configuration for APScheduler
 class Config:
     SCHEDULER_API_ENABLED = True
-app = Flask(__name__)
+
 app.config.from_object(Config())
+
+# Initialize the scheduler
 scheduler = APScheduler()
-REQUEST_HANDLER = ''
+scheduler.init_app(app)
+scheduler.start()
 
-def run_drl_task_local():
-    REQUEST_HANDLER.wfile.write('class handler(BaseHTTPRequestHandler):'.encode('utf-8'))
-    current_time = datetime.now().time()
-    REQUEST_HANDLER.wfile.write(f'Current Time: {current_time}'.encode('utf-8'))
- 
- 
-class handler(BaseHTTPRequestHandler):
-# When a GET request is received, this method is automatically called.
-    def do_GET(self):
-        # Sends a 200 OK response status code to indicate the request was successful.
-        REQUEST_HANDLER = self
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write(REQUEST_HANDLER.encode('utf-8'))
-        run_drl_task_local(self)
-        return
-def scheduled_task():
-    run_drl_task_local()    
-    
+# Define a job to run every 5 seconds
+@scheduler.task('interval', id='show_time', seconds=5, misfire_grace_time=900)
+def show_time():
+    print(f"Current time: {datetime.now()}")
+
 if __name__ == '__main__':
-    scheduler.init_app(app)
-    scheduler.start()
-    # Schedule the task to run every 10 seconds
-    scheduler.add_job(id='Scheduled Task', func=scheduled_task, trigger='interval', seconds=10)
-    app.run(debug=True)    
-
+    app.run(debug=True)
