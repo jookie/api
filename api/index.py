d018f55d411a42aeb42c1a33ecb9d7f2bd6730ce
jookie <dovpeles@gmail.com>
2024-08-16 14:56:26 -0400

    fix commit

--- api/index.py
+++ api/index.py
@@ -2,36 +2,37 @@ from flask import Flask
 from flask_apscheduler import APScheduler
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
-
-app = Flask(__name__)
-
-# Configuration for APScheduler
 class Config:
     SCHEDULER_API_ENABLED = True
-
+app = Flask(__name__)
 app.config.from_object(Config())
-
-# Initialize the scheduler
 scheduler = APScheduler()
-scheduler.init_app(app)
-scheduler.start()
+REQUEST_HANDLER = None
 
-# Define a job to run at regular intervals
-@scheduler.task('interval', id='do_job_1', seconds=10, misfire_grace_time=900)
-def job1():
-    print(f"Task running at {datetime.now()}")
-
-# Define the BaseHTTPRequestHandler to handle HTTP requests
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+def run_drl_task_local():
+    REQUEST_HANDLER.wfile.write('class handler(BaseHTTPRequestHandler):'.encode('utf-8'))
+    current_time = datetime.now().time()
+    REQUEST_HANDLER.wfile.write(f'Current Time: {current_time}'.encode('utf-8'))
+ 
+ 
+class handler(BaseHTTPRequestHandler):
+# When a GET request is received, this method is automatically called.
     def do_GET(self):
+        # Sends a 200 OK response status code to indicate the request was successful.
+        REQUEST_HANDLER = self
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(b"Hello, World! This is a GET response.")
-
-@app.route('/')
-def index():
-    return "Welcome to the Flask App with APScheduler!"
-
+        self.wfile.write('doGet:'.encode('utf-8'))
+        run_drl_task_local(self)
+        return
+def scheduled_task():
+    run_drl_task_local()    
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    scheduler.init_app(app)
+    scheduler.start()
+    # Schedule the task to run every 10 seconds
+    scheduler.add_job(id='Scheduled Task', func=scheduled_task, trigger='interval', seconds=10)
+    app.run(debug=True)    
+
