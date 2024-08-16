6d50392f1359ec52aff379a658699ee143160057
jookie <dovpeles@gmail.com>
2024-08-16 10:27:51 -0400

    fix commit

--- examples/main.py
+++ examples/main.py
@@ -1,6 +1,6 @@
 #  DRL with Alpaca to place a buy order
-def handler(request, response):
-    with open("data/hello.txt", "w") as file:
-        file.write("step-by-step detailed guide and code")
-    return response.status(200).send("File created successfully!")
+# def handler(request, response):
+#     with open("data/hello.txt", "w") as file:
+#         file.write("step-by-step detailed guide and code")
+#     return response.status(200).send("File created successfully!")
 
