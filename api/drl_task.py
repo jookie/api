6e2a5c6e231ebd2511df502c818eba1ba40ed4c9
jookie <dovpeles@gmail.com>
2024-08-16 13:57:50 -0400

    fix commit

--- api/drl_task.py
+++ api/drl_task.py
@@ -1,3 +1,4 @@
 def run_drl_task(request_handler, msg):
-    request_handler.wfile.write('run_drl_task()from req handler '.encode('utf-8'))
+    # request_handler.wfile.write('run_drl_task()from req handler '.encode('utf-8'))
     # Add your DRL script logic here
+    request_handler.wfile.write(msg.encode('utf-8'))
--- api/index.py
+++ api/index.py
@@ -2,11 +2,11 @@
 # from flask_apscheduler import APScheduler
 # from drl_task import run_drl_task  # Ensure this function is defined in drl_task.py
 from http.server import BaseHTTPRequestHandler
-# from drl_task import run_drl_task_external
+from drl_task import Drl
 
 def run_drl_task_local(request_handler):
     request_handler.wfile.write('run_drl_task_local'.encode('utf-8'))
-    # run_drl_task_external(request_handler, 'run_drl_task_external() from req handler')
+    Drl(request_handler, 'req handler')
     
 class handler(BaseHTTPRequestHandler):
 # When a GET request is received, this method is automatically called.
